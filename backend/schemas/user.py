5dc3ef5cacd4c025af262dfe171ca99ea6421c58
a0966082181 <143152436+a0966082181@users.noreply.github.com>
2025-05-05 07:55:37 +0800

    Create user.py

---
+++ backend/schemas/user.py
@@ -0,0 +1,10 @@
+# backend/schemas/user.py
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
