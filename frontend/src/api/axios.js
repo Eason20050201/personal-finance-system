7d5211be0f57fc12f3c680f4b04c30b43fe9bdbb
WillyLin <linliqi20031118@gmail.com>
2025-06-11 10:28:23 +0800

    change baseurl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://mariah-wildlife-perspective-loved.trycloudflare.com',
+  baseURL: 'https://calculators-feat-saturn-airplane.trycloudflare.com',
   withCredentials: true
 })
 
