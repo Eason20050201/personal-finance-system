5f0de30b72f0578ba29123fc93b04880fe2e4da5
WillyLin <linliqi20031118@gmail.com>
2025-06-13 22:25:18 +0800

    change baseurl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://figured-lawyers-flux-prime.trycloudflare.com',
+  baseURL: 'https://customize-ips-deeper-drugs.trycloudflare.com',
   withCredentials: true
 })
 
